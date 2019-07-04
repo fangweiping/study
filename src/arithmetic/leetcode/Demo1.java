1aa3e368cfbe8a4464a95b116252d2173d9ad3d5
fwp <625881050@qq.com>
2019-07-05 00:27:41 +0800

    添加dmeo1文件

--- src/arithmetic/leetcode/Demo1.java
+++
@@ -1,4 +0,0 @@
-package arithmetic.leetcode;
-
-public class Demo1 {
-}
--- src/arithmetic/leetcode/Demo2.java
+++ src/arithmetic/leetcode/Demo2.java
@@ -1,4 +1,9 @@
 package arithmetic.leetcode;
 
 public class Demo2 {
+
+    public static void main(String[] args) {
+
+
+    }
 }
