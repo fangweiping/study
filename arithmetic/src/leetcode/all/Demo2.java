963cb5834813ae9f2b290ebf4a75171cb5b26cd8
fwplove <625881050@qq.com>
2019-07-16 13:48:00 +0800

    新增分类

--- arithmetic/src/leetcode/all/Demo2.java
+++ arithmetic/src/leetcode/all/Demo2.java
@@ -1,5 +1,7 @@
 package leetcode.all;
 
+import po.ListNode;
+
 /**
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
  *
@@ -13,13 +15,15 @@ package leetcode.all;
  * 输出：7 -> 0 -> 8
  * 原因：342 + 465 = 807
  */
-//public class Demo2 {
-//
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//
-////    }
-//}
+public class Demo2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        return null;
+    }
+}
