9c43677f6a2a8a587d84aaea255dd63101efbeae
fwplove <625881050@qq.com>
2019-08-02 16:50:34 +0800

    文件上传

--- aopdemo/src/main/java/com/fwp/demo/controller/UserController.java
+++ aopdemo/src/main/java/com/fwp/demo/controller/UserController.java
@@ -3,15 +3,15 @@ package com.fwp.demo.controller;
 import com.alibaba.fastjson.JSON;
 import com.fwp.demo.aop.annotation.Login;
 import com.fwp.demo.vo.RespResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.omg.IOP.Encoding;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
 import java.net.URLEncoder;
 
 @RestController
@@ -58,5 +58,21 @@ public class UserController {
         resp.setMsg("登陆成功");
         resp.setData(null);
         return JSON.toJSONString(resp);
+    }
+
+    @PostMapping("upload")
+    public String uploadFile(@RequestParam("file")MultipartFile file,HttpServletRequest request) throws IOException {
+        byte[] bytes = file.getBytes();
+
+        InputStreamReader isr = new InputStreamReader(file.getInputStream(),"utf-8");
+        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(new File("d:\\" + file.getOriginalFilename())),"utf-8");
+        char[] chars = new char[1024] ;
+        while (isr.read(chars)!=-1){
+            osw.write(chars);
+            osw.flush();
+        }
+        osw.close();
+        isr.close();
+        return null;
     }
 }
