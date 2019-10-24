9a65d8c8db2d2fa526d5785c5f70efc47c767677
FWP <625881050@qq.com>
2019-10-24 20:21:42 +0800

    增加filecontroller

--- aopdemo/src/main/java/com/fwp/demo/ApplicationDemo.java
+++ aopdemo/src/main/java/com/fwp/demo/ApplicationDemo.java
@@ -3,6 +3,7 @@ package com.fwp.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApplicationDemo {
     public static void main(String[] args) {
---
+++ aopdemo/src/main/java/com/fwp/demo/controller/FileController.java
@@ -0,0 +1,111 @@
+package com.fwp.demo.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.fwp.demo.vo.RespResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("file")
+public class FileController {
+
+    @CrossOrigin("https://www.cnblogs.com")
+    @GetMapping("resource")
+    public String getResourceFile(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //获取资源路径
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("files\\1.csv");
+        //获取输出流
+        ServletOutputStream outputStream = response.getOutputStream();
+        // 配置文件下载
+        response.setHeader("content-type", "application/octet-stream");
+        //application/octet-stream可以理解为通用,具体文件类型通过给出的扩展名来确定
+        //response.setContentType("text/cvs");
+         //扩展名编码处理,扩展名即是文件名
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("1.csv", "UTF-8"));
+
+        //缓冲数组
+        byte[] buf = new byte[1024];
+        while (resourceAsStream.read(buf) != -1) {
+            outputStream.write(buf);
+        }
+        //关闭资源
+        outputStream.close();
+        resourceAsStream.close();
+
+        RespResult resp = new RespResult();
+        resp.setCode(200);
+        resp.setMsg("登陆成功");
+        resp.setData(null);
+        return JSON.toJSONString(resp);
+    }
+
+
+    /**
+     * 文件上传
+     * @param file
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String charset = null;
+        //基本上文件上传除了 gbk 就是utf-8    ,gbk 就是ansi
+        byte[] b = file.getBytes();
+        if (b[0] == -17 && b[1] == -69 && b[2] == -65) {
+            charset = "utf-8";
+        } else {
+            charset = "gbk";
+        }
+        InputStreamReader isr = new InputStreamReader(file.getInputStream(), charset);
+        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(new File("d:\\" + file.getOriginalFilename())), "utf-8");
+        char[] chars = new char[1024];
+        while (isr.read(chars) != -1) {
+            osw.write(chars);
+            osw.flush();
+        }
+        osw.close();
+        isr.close();
+        return null;
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    @GetMapping("download")
+    public String getU8File(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 配置文件下载
+        response.setHeader("content-type", "application/octet-stream");
+        response.setContentType("application/octet-stream");
+        // 下载文件能正常显示中文
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("1.csv", "utf-8"));
+        OutputStream os = response.getOutputStream();
+
+        //该list保存每一行聊天数据
+        List<String> content = new ArrayList<>();
+        content.add("你好");
+        content.add("我好");
+        OutputStreamWriter osw = new OutputStreamWriter(os, "gbk");
+        for (String s : content) {
+            osw.write(s);
+            osw.write(System.lineSeparator());
+            osw.flush();
+        }
+        //关闭资源
+        //.....
+        return null;
+    }
+}
