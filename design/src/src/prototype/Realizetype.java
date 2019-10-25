4864b300a28b1a46fffc00431de801ec098be71a
fwplove <625881050@qq.com>
2019-10-26 00:57:21 +0800

    新增原型设计模式代码

---
+++ design/src/src/prototype/Realizetype.java
@@ -0,0 +1,42 @@
+package src.prototype;
+
+/**
+ * 1.抽象原型类：规定了具体原型对象必须实现的接口。 这里就是 Cloneable 接口
+ *2.具体原型类：实现抽象原型类的 clone() 方法，它是可被复制的对象。
+ */
+public class Realizetype  implements  Cloneable{
+     private int age ;
+     private String name;
+
+    public Realizetype() {
+
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+        return (Realizetype)super.clone();
+    }
+
+    public Realizetype(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
