c460d3cb14d5840b130e87edc51296140788d13b
fwplove <625881050@qq.com>
2019-11-22 12:23:47 +0800

    算法27

--- arithmetic/src/leetcode/all/Demo27.java
+++ arithmetic/src/leetcode/all/Demo27.java
@@ -46,4 +46,26 @@ public class Demo27 {
         }
         return length;
     }
+
+
+    /**
+     * 双指针法!
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static  int removeElement1(int[] nums, int val) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] == val) {
+                for (int j = i; j < nums.length-1; j++) {
+                    nums[j] = nums[j+ 1];
+                }
+                length--;
+                i--;
+            }
+        }
+        return length;
+    }
 }
