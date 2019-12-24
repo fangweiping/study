69c5fc341944944c50b4631c66bf9741f8f5c229
fwplove <625881050@qq.com>
2019-12-24 11:25:15 +0800

    修改包结构

--- arithmetic/src/lambda/Demo1.java
+++ arithmetic/src/lambda/Demo1.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 
+
+
 /**
  * 需求:
  * 获取相同年龄中成绩最高的学生
@@ -15,12 +17,7 @@ public class Demo1 {
     public static void main(String[] args) {
         ArrayList<User> list = new ArrayList<>();
 
-        list.add(new User(10, 88));
-        list.add(new User(10, 34));
-        list.add(new User(9, 22));
-        list.add(new User(9, 32));
-        list.add(new User(8, 44));
-        list.add(new User(8, 11));
+
 
         Map<Integer, List<User>> map = list.stream().sorted(((o1, o2) -> o2.getGrade() - o1.getGrade())).collect(Collectors.groupingBy(User::getAge));
         System.out.println(map);
