a5a088676c12a6b343c029ca77380672bba993e5
fwplove <625881050@qq.com>
2020-01-02 19:28:28 +0800

    新增工具类

---
+++ myutils/src/CommonTool.java
@@ -0,0 +1,34 @@
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CommonTool {
+
+    /**
+     * 判断字符串是否为null或""
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+        if(str==null||str.trim().isEmpty())return true;
+        return false;
+    }
+
+    /**
+     * 判断集合是否为null或空集合
+     * @param collection
+     * @return
+     */
+    public static boolean isEmpty(Collection collection) {
+        if(collection==null||collection.isEmpty())return true;
+        return false;
+    }
+
+    /**
+     * 判断map是否为null或空map
+     */
+    public static boolean isEmpty(Map map) {
+        if(map==null||map.isEmpty())return true;
+        return false;
+    }
+}
