37751606f91743ec50bb0eb28bef66396703909d
fwplove <625881050@qq.com>
2020-08-20 04:26:05 +0800

    springboot整合jpa

---
+++ springboot-jpa/src/main/java/com/fwp/demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.fwp.demo.repository;
+
+import com.fwp.demo.entity.User;
+
+public interface UserRepository extends BaseRepository<User,Long> {
+
+
+}
---
+++ springboot-jpa/src/main/java/com/fwp/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.fwp.demo.service.impl;
+
+import com.fwp.demo.repository.UserRepository;
+import com.fwp.demo.entity.User;
+import com.fwp.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
