6992b63595b4b527bfdc87c011e99370b45f5ae6
fwplove <625881050@qq.com>
2021-06-03 10:32:58 +0800

    修改

--- springboot-mybatis/src/main/java/com/fwp/demo/controller/OrderController.java
+++ springboot-mybatis/src/main/java/com/fwp/demo/controller/OrderController.java
@@ -2,11 +2,13 @@ package com.fwp.demo.controller;
 
 import com.fwp.demo.service.OrderService;
 import com.fwp.demo.entity.Order;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/order")
@@ -15,8 +17,13 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private HikariDataSource hikariDataSource;
+
     @GetMapping("/detail/{orderNum}")//http://localhost:8080/order/detail/20140921001
     public Order queryOrderDetail(@PathVariable Long orderNum) {
+        long maxLifetime = hikariDataSource.getMaxLifetime();
+        System.out.println("maxLifetime = " + maxLifetime);
         return orderService.queryOrderDetail(orderNum);
     }
 }
