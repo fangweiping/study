8fb5ebf263a19159d8468fc2a1e407d8032c5087
fwplove <625881050@qq.com>
2024-06-23 07:19:38 +0800

    zuul搭建

--- springboot-zuul/src/main/java/com/fwp/study/ZuulApplication.java
+++ springboot-zuul/src/main/java/com/fwp/study/ZuulApplication.java
@@ -2,12 +2,12 @@ package com.fwp.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
-public class StudyApplication {
+public class ZuulApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StudyApplication.class, args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 }
