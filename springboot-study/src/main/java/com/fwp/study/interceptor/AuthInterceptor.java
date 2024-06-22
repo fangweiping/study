d12ba7116174637356728e2cc6c51b3d9c876897
fwplove <625881050@qq.com>
2024-06-22 19:22:20 +0800

    拦截器+ThreadLocal存储用户信息方便后续业务逻辑直接获取用户信息,主要是用户id和工号等

--- springboot-study/src/main/java/com/fwp/study/interceptor/AuthInterceptor.java
+++ springboot-study/src/main/java/com/fwp/study/interceptor/AuthInterceptor.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //1.获取token
